caaa10ba2335181105be313067fd49ec9b9b5fd3
quyHoang99k <quyhoang99k@gmail.com>
2021-10-27 13:18:04 +0700

    added redux,and implemented userReducer and userActions to header and app component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Switch, Route } from 'react-router';
-
+import { connect } from 'react-redux';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop';
 import Header from './components/header/header';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up';
-import { auth } from './firebase/firebase.utils';
-
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
+import { setCurrentUser } from './redux/user/user.action';
 
 
 
@@ -16,28 +16,35 @@ import './sass/index.scss'
 
 class App extends React.Component {
 
-  constructor(){
-    super()
-
-    this.state = {
-      currentUser: null
-    }
-  }
   unsubscribeFromAuth = null
-  componentDidMount(){
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user =>{
-      this.setState({currentUser:user})
-      console.log("user", user)
-    })
+  componentDidMount() {
+    const { setCurrentUser } = this.props;
+
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+
+        userRef.onSnapshot(snapShot => {
+          setCurrentUser({
+            id: snapShot.id,
+            ...snapShot.data()
+          });
+        });
+      }
+
+      setCurrentUser(userAuth);
+    });
   }
-  componentWillUnmount(){
+
+
+  componentWillUnmount() {
     this.unsubscribeFromAuth()
   }
 
   render() {
     return (
       <div>
-        <Header currentUser={this.state.currentUser} />
+        <Header />
         <Switch>
           <div >
             <Route exact path='/' component={HomePage} />
@@ -49,5 +56,7 @@ class App extends React.Component {
     );
   }
 }
-
-export default App;
+const mapDispatchToProps = dispatch => ({
+  setCurrentUser: user => dispatch(setCurrentUser(user))
+})
+export default connect(null, mapDispatchToProps)(App);
--- src/pages/sign-in-and-sign-up/sign-in-and-sign-up.jsx
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import Signin from "../../components/sign-in/sign-in";
+import SignUp from "../../components/sign-up/sign-up";
 
 const SignInAndSignUpPage = () =>(
     <div className="sign-in-and-sign-up">
         <Signin />
+        <SignUp/>
     </div>
 )
 export default SignInAndSignUpPage
